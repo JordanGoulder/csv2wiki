599754a9a11003815238b272c30f44a11f897471
Jordan Goulder <jordan.goulder@gmail.com>
2012-03-06 10:57:06 -0500

    Added license

--- main.c
+++ main.c
@@ -1,3 +1,30 @@
+/*
+ * csv2wiki
+ *
+ * This pogram will convert a comma seperated value (.csv) file into
+ * the wiki syntax used by MediaWiki.  This input is read from stdin
+ * and the output is written to stdout.
+ *
+ * Usage:
+ *        csv2wiki < input.csv > output.wiki
+ *
+ *
+ * Copyright (c) 2012 Jordan Goulder. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 static void table_begin(void);
